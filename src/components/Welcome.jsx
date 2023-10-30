007b92a86971c3f6dc38c1ba9d34f2d562e341a1
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2023-10-30 16:52:03 +0100

    Aggiornato welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,8 @@
 function Welcome() {
     return (
      <div className="mt-4 p-5 text-white rounded jumbotron">
-        <h1>Fantasy Books</h1>
-        <p>Lorem ipsum...</p>
+        <h1>EpiBooks</h1>
+        <p>Here you can find all the books by genre.</p>
      </div>
     );
 }
